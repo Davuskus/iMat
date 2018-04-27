aac4ee0b6942a146ab22b13f0b64bbcca34a9395
Davuskus <welexay@gmail.com>
2018-04-27 23:19:30 +0200

    Small changes.

--- src/imat/Main.java
+++ src/imat/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     // Backend JavaDoc: http://www.cse.chalmers.se/research/group/idc/ituniv/courses/18/dkgg/p/backend/javadoc/
@@ -14,7 +16,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("imat.fxml"));
         primaryStage.setTitle("iMat");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+
+        double ratio = 1.0 / 2;
+        int stageWidth = (int) (0.5 + Toolkit.getDefaultToolkit().getScreenSize().width * ratio);
+        int stageHeight = (int) (0.5 + Toolkit.getDefaultToolkit().getScreenSize().height * ratio);
+
+        primaryStage.setScene(new Scene(root, stageWidth, stageHeight));
         primaryStage.show();
     }
 
