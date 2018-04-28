f750ef9deefaf094c924a2d17b128c9be4bbf0d9
Elias Sundqvist <elias.jan730+github@gmail.com>
2018-04-28 20:57:18 +0200

    log searchbox input

--- src/imat/controls/header/Header.java
+++ src/imat/controls/header/Header.java
@@ -1,9 +1,19 @@
 package imat.controls.header;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class Header extends AnchorPane {
-    public Header(){
-        imat.utils.FXMLLoader.loadFXMLFromRootPackage("header.fxml",this, this);
+public class Header {
+    @FXML
+    private TextField searchBox;
+
+    @FXML
+    private void onSearchBoxUpdate(InputEvent event) {
+        System.out.println(searchBox.getText());
     }
+
 }
