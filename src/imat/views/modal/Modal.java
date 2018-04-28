7a2e6104131256071aa71a62f7afa91f6578038b
Davuskus <davuskuscode@gmail.com>
2018-04-28 21:16:55 +0200

    Made the modal panel consume mouse click events.

--- src/imat/views/modal/Modal.java
+++ src/imat/views/modal/Modal.java
@@ -1,4 +1,13 @@
 package imat.views.modal;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
 public class Modal {
+
+    @FXML
+    public void consumeEvent(Event event) {
+        event.consume();
+    }
+
 }
