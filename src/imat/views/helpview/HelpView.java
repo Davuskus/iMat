1458357002f44bb9344cba1d00fb21d10fc18fda
Carl Holmberg <ellharholmberg@outlook.com>
2018-04-29 12:59:40 +0200

    Utökade metoder i hjälpvyn

--- src/imat/views/helpview/HelpView.java
+++ src/imat/views/helpview/HelpView.java
@@ -1,10 +1,13 @@
 package imat.views.helpview;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
+import imat.utils.FXMLLoader;
 
 public class HelpView extends AnchorPane {
     @FXML
@@ -16,12 +19,53 @@ public class HelpView extends AnchorPane {
     @FXML
     WebView helpTextView;
 
-    public void loadHtmlTextFromFile(String htmlPath) {
+    AnchorPane parentWindow;
+
+    public HelpView() {
+        FXMLLoader.loadFXMLFromRootPackage("helpView.fxml", this, this);
+        loadHtmlTextFromFile("res/helpText.html");
+
+    }
+
+    private void loadHtmlTextFromFile(String htmlPath) {
         String htmlContent = imat.utils.FileUtils.readAllTextFromFile(htmlPath);
         loadHtmlTextFromString(htmlContent);
     }
 
-    public void loadHtmlTextFromString(String htmlContent) {
+    private void loadHtmlTextFromString(String htmlContent) {
         helpTextView.getEngine().loadContent(htmlContent, "text/html");
+    }
+
+    /*
+    @FXML
+    public void closeHelpView() {
+
+    }
+    */
+
+    @FXML
+    public void mouseTrap(Event event) {
+        event.consume();
+    }
+
+    @FXML
+    public void closeButtonMouseEntered() {
+        helpViewCloseButton.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "../res/images/icons/close/icon_close_hover.png"
+        )));
+    }
+
+    @FXML
+    public void closeButtonMousePressed() {
+        helpViewCloseButton.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "../res/images/icons/close/icon_close_pressed.png"
+        )));
+    }
+
+    @FXML
+    public void closeButtonMouseExited() {
+        helpViewCloseButton.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "../res/images/icons/close/icon_close_close.png"
+        )));
     }
 }
