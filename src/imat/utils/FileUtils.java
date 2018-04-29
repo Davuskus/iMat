b64e486da307977c184e6209859e21f94654f9ad
Carl Holmberg <ellharholmberg@outlook.com>
2018-04-29 10:33:09 +0200

    Skapat fxml och kontroller för bekräftelseruta, lagt till lite javadoc för utils

--- src/imat/utils/FileUtils.java
+++ src/imat/utils/FileUtils.java
@@ -4,8 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ *  Utility class for reading and writing files
+ */
+
 public class FileUtils {
 
+    /**
+     *  Reads all text from a file, including newlines, returning it as a single string
+     * @param path Path to a text file
+     * @return The full file content (ascii encoding)
+     */
     public static String readAllTextFromFile(String path) {
         StringBuilder contentBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(path)))
---
+++ src/imat/views/payment/Confirmation.java
@@ -0,0 +1,5 @@
+package imat.views.payment;
+
+public class Confirmation {
+
+}
