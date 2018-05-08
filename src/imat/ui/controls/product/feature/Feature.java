3b7c7cbab31592fd4f6d3a1e9b13ca6e95383448
Davuskus <welexay@gmail.com>
2018-05-08 16:23:13 +0200

    Started working on the home screen UI.

--- src/imat/ui/controls/product/feature/Feature.java
+++ src/imat/ui/controls/product/feature/Feature.java
@@ -1,4 +1,34 @@
 package imat.ui.controls.product.feature;
 
-public class Feature {
+import imat.enums.NavigationTarget;
+import imat.model.FXMLController;
+import javafx.fxml.FXML;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Feature extends FXMLController {
+
+    private int currentProductIndex;
+    private final List<Product> products;
+
+    public Feature() {
+        super();
+        products = new ArrayList<>(4);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void showProductDetails() {
+        model.showProductDetails(products.get(currentProductIndex));
+        model.navigate(NavigationTarget.PRODUCT_DETAILS);
+    }
+
 }
