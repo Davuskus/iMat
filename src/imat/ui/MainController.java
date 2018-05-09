26912d041fb763e4f1566bfc31fcdb55812a18bd
Davuskus <davuskuscode@gmail.com>
2018-05-09 22:01:00 +0200

    Checkout is now in a StackPane in Browse. Removed a redundant Checkout-controller class and its FXML file which only included another checkout FXML file.

--- src/imat/ui/MainController.java
+++ src/imat/ui/MainController.java
@@ -5,12 +5,8 @@ import imat.interfaces.INavigationListener;
 import imat.model.FXMLController;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
-import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.Product;
-import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainController extends FXMLController implements INavigationListener {
@@ -19,48 +15,17 @@ public class MainController extends FXMLController implements INavigationListene
     private AnchorPane modalView;
 
     @FXML
-    private AnchorPane checkoutPane;
-
-    @FXML
     private AnchorPane browseView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // placeRandomOrder(getRandomInteger(1, 15));
         model.addNavigationListener(this);
         model.navigate(NavigationTarget.HOME);
-    }
-
-    // Temporary
-    private int getRandomInteger(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
-    }
-
-    // Temporary
-    private void placeRandomOrder(int numberOfPurchases) {
-        if (!IMatDataHandler.getInstance().getProducts().isEmpty()) {
-            for (int i = 0; i < numberOfPurchases; i++) {
-                ShoppingItem shoppingItem = new ShoppingItem(getRandomProduct());
-                shoppingItem.setAmount(getRandomInteger(1, 20));
-                IMatDataHandler.getInstance().getShoppingCart().addItem(shoppingItem);
-            }
-            IMatDataHandler.getInstance().placeOrder(true);
-        }
-    }
-
-    // Temporary
-    private Product getRandomProduct() {
-        int randomId = getRandomInteger(1, IMatDataHandler.getInstance().getProducts().size());
-        return IMatDataHandler.getInstance().getProduct(randomId);
     }
 
     @Override
     public void navigateTo(NavigationTarget navigationTarget) {
         switch (navigationTarget) {
-            case CHECKOUT:
-                checkoutPane.toFront();
-                break;
             case HELP:
             case COPY_ORDER:
             case CONFIRMATION:
@@ -70,6 +35,7 @@ public class MainController extends FXMLController implements INavigationListene
             case HOME:
             case PRODUCT_DETAILS:
             case ORDER_HISTORY:
+            case CHECKOUT:
                 browseView.toFront();
             default:
                 browseView.toFront();
