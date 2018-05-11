d87faa1bc4419f0b0d376170476ca86d6c89474d
Davuskus <davuskuscode@gmail.com>
2018-05-11 14:51:00 +0200

    Added a new class called FeatureItem and a related FXML file as well.

---
+++ src/imat/ui/controls/product/feature/item/FeatureItem.java
@@ -0,0 +1,14 @@
+package imat.ui.controls.product.feature.item;
+
+import imat.model.FXMLController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FeatureItem extends FXMLController {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
