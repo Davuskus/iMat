020ee3fe5b4af4ef271b3b23f9eb4cb1c8a3fa75
Davuskus <davuskuscode@gmail.com>
2018-05-12 13:40:00 +0200

    Made the CheckBox for ecological products functional.

--- src/imat/ui/views/browse/centerviews/products/Products.java
+++ src/imat/ui/views/browse/centerviews/products/Products.java
@@ -6,8 +6,10 @@ import imat.model.FXMLController;
 import imat.model.category.Category;
 import imat.ui.controls.product.menu.ProductMenuItem;
 import imat.utils.FXMLLoader;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.Product;
@@ -24,8 +26,14 @@ public class Products extends FXMLController implements ICategoryListener, ISear
     @FXML
     private FlowPane productsFlowPane;
 
-    //private ProductCategory currentCategory;
+    @FXML
+    private CheckBox onlyEcoCheckBox;
+
     private Category currentCategory;
+
+    private boolean onlyEcologicalProducts;
+
+    private List<Product> currentProducts;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,7 +47,7 @@ public class Products extends FXMLController implements ICategoryListener, ISear
         currentCategory = category;
         productsFlowPane.getChildren().removeIf(x -> true);
         categoryLabel.setText(category.getName());
-        populateWithProducts(category.getAllProducts());
+        populateWithProducts(category.getAllProducts(), onlyEcologicalProducts);
     }
 
     @Override
@@ -53,17 +61,27 @@ public class Products extends FXMLController implements ICategoryListener, ISear
 
         categoryLabel.setText(categoryText);
         productsFlowPane.getChildren().removeIf(x -> true);
-        populateWithProducts(products);
+        populateWithProducts(products, onlyEcologicalProducts);
     }
 
-    private void populateWithProducts(List<Product> products) {
+    private void populateWithProducts(List<Product> products, boolean onlyEcologicalProducts) {
+        currentProducts = products;
         for (Product product : products) {
-            ProductMenuItem controller = new ProductMenuItem(product);
-            controller.setModel(model);
-            String fxmlPath = "../../../../controls/product/menu/product_menu_item.fxml";
-            Node item = FXMLLoader.loadFXMLNodeFromRootPackage(fxmlPath, this, controller);
-            productsFlowPane.getChildren().add(item);
+            if (onlyEcologicalProducts && product.isEcological() || !onlyEcologicalProducts) {
+                ProductMenuItem controller = new ProductMenuItem(product);
+                controller.setModel(model);
+                String fxmlPath = "../../../../controls/product/menu/product_menu_item.fxml";
+                Node item = FXMLLoader.loadFXMLNodeFromRootPackage(fxmlPath, this, controller);
+                productsFlowPane.getChildren().add(item);
+            }
         }
+    }
+
+    @FXML
+    private void checkBoxOnAction(Event event) {
+        onlyEcologicalProducts = onlyEcoCheckBox.isSelected();
+        productsFlowPane.getChildren().removeIf(x -> true);
+        populateWithProducts(currentProducts, onlyEcologicalProducts);
     }
 
 }
