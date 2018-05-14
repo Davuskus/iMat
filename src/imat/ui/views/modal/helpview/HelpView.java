9054f3fcf979da33ed2bcfbd174bfa670b2ffc40
carhol <carhol@student.chalmers.e>
2018-05-14 13:23:35 +0200

    Fixed helpview scrolling. Again.

--- src/imat/ui/views/modal/helpview/HelpView.java
+++ src/imat/ui/views/modal/helpview/HelpView.java
@@ -4,7 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
-
+import javafx.scene.web.WebEngine;
+import netscape.javascript.JSObject;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,6 +16,8 @@ public class HelpView implements Initializable {
     @FXML
     WebView helpTextView;
 
+    WebEngine webEngine;
+
     @FXML
     AnchorPane basePane;
 
@@ -22,13 +25,14 @@ public class HelpView implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         //FXMLLoader.loadFXMLFromRootPackage("helpView.fxml", this, this);
         //loadHtmlTextFromFile("src/imat/resources/helptext/helptext.html");
+        webEngine = helpTextView.getEngine();
         loadHtmlTextFromFile("src/imat/resources/helptext/helptext.html");
         helpTextView.getEngine().setUserStyleSheetLocation(getClass().getResource("../../../../resources/helptext/helpStyle.css").toString());
     }
 
     private void loadHtmlTextFromFile(String htmlPath) {
         String htmlContent = imat.utils.FileUtils.readAllTextFromFile(htmlPath);
-        helpTextView.getEngine().loadContent(htmlContent, "text/html");
+        webEngine.loadContent(htmlContent, "text/html");
     }
 
     private void setStyleFromFile(String cssPath) {
